465341cb13cdf815a946ebff63f4b58215c88c34
Sangjin <smithsk013@gmail.com>
2018-03-08 17:30:31 -0500

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,30 +2,30 @@
 
 #import dependencies
 import pandas as pd
-
+import sys
 
 #Read CSV
 pybank = pd.read_csv("raw_data/budget_data_1.csv")
 
 
 #Print Total number of months
-pydate = pybank[["Date"]].count()
+pydate = pybank["Date"].count()
 
 
 #Print Total Revenue
-pyrev = pybank[["Revenue"]].sum()
+pyrev = pybank["Revenue"].sum()
 
 
 #Print Average Revenue Change
-pyavgrev = pybank[["Revenue"]].mean()
+pyavgrev = pybank["Revenue"].mean()
 
 
 #The Greatest Increase in Revenue (date and amount) over the entire period
-bankmax = pybank[["Revenue"]].max()
+bankmax = pybank["Revenue"].max()
 
 
 #The greatest decrease in revenue (date and amount) over the entire period
-bankmin = pybank[["Revenue"]].min()
+bankmin = pybank["Revenue"].min()
 
 #Print the set values
 print("Financial Analysis")
@@ -38,4 +38,12 @@ print("Greatest Decrease in Revenue: " + str(bankmin))
 
 
 #Export a file with the results
+with open("PyBankResults.txt", "w") as text_file:
+    text_file.write("Financial Analysis\n")
+    text_file.write("---------------------\n")
+    text_file.write("Total Months: " + str(pydate) + "\n")
+    text_file.write("Total Revenue         : $" + str(pyrev) + "\n")
+    text_file.write("Average Revenue       : $" + str(pyavgrev) + "\n")
+    text_file.write("Greatest Increase in Revenue: " + str(bankmax) + "\n")
+    text_file.write("Greatest Decrease in Revenue: " + str(bankmin) + "\n")
 
